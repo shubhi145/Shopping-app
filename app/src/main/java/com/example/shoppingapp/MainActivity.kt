5c9503916e9b9b423e2f39c476c6fab3f0f88694
shubhi145 <shubhiagrawal145@gmail.com>
2025-05-18 18:05:34 +0530

    3rd commit

--- app/src/main/java/com/example/shoppingapp/MainActivity.kt
+++ app/src/main/java/com/example/shoppingapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.shoppingapp.presentation.LoginScreenUi
 import com.example.shoppingapp.presentation.SignUpScreen
 import com.example.shoppingapp.ui.theme.ShoppingAppTheme
 
@@ -21,7 +22,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ShoppingAppTheme {
 
-                SignUpScreen()
+                LoginScreenUi()
             }
         }
     }
--- app/src/main/java/com/example/shoppingapp/presentation/LoginScreen.kt
+++ app/src/main/java/com/example/shoppingapp/presentation/LoginScreen.kt
@@ -1,7 +1,9 @@
 package com.example.shoppingapp.presentation
 
 import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,78 +15,53 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Phone
-import androidx.compose.material.icons.filled.Place
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.shoppingapp.R
 import com.example.shoppingapp.presentation.Utils.CustomTextFiled
 
-@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun SignUpScreen(){
+fun LoginScreenUi(){
+
     val context = LocalContext.current
-    var firstName by remember{
-        mutableStateOf("")
-    }
-    var lastName by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
-    var confirmPassword by remember { mutableStateOf("") }
-    var phoneNumber by remember { mutableStateOf("") }
 
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-       Text(text = "SignUp",
-           fontWeight = FontWeight.Bold,
-           fontSize = 24.sp,
-           modifier = Modifier
-               .padding(vertical = 30.dp)
-               .align(Alignment.Start)
-       )
-        CustomTextFiled(
-            value = lastName,
-            onValueChange = {lastName = it},
-            label = "Last Name",
-            leadingIcon = Icons.Default.Person,
+    Column (
+        modifier = Modifier.fillMaxSize().padding(16.dp)
+            , horizontalAlignment = Alignment.CenterHorizontally
+        , verticalArrangement = Arrangement.Center
+
+    ){
+        Text(text = "Login",
+            fontSize = 24.sp,
+            style = TextStyle(fontWeight = FontWeight.Bold),
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 6.dp),
-        )
-        CustomTextFiled(
-            value = firstName,
-            onValueChange = {firstName = it},
-            label = "First Name",
-            leadingIcon = Icons.Default.Person,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 6.dp),
-        )
+                .padding(16.dp)
+                .align(Alignment.Start)
+          )
+
         CustomTextFiled(
             value = email,
             onValueChange = {email = it},
@@ -95,67 +72,49 @@ fun SignUpScreen(){
                 .padding(vertical = 6.dp),
         )
         CustomTextFiled(
-            value = phoneNumber,
-            onValueChange = {phoneNumber = it},
-            label = "Phone Number",
-            leadingIcon = Icons.Default.Phone,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 6.dp),
-        )
-        CustomTextFiled(
             value = password,
             onValueChange = {password = it},
-            label = "Create Password",
+            label = "Password",
             leadingIcon = Icons.Default.Lock,
+            visualTransformation = PasswordVisualTransformation(),
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(vertical = 6.dp),
-            visualTransformation = PasswordVisualTransformation(),
         )
-        CustomTextFiled(
-            value = confirmPassword,
-            onValueChange = {confirmPassword = it},
-            label = "Confirm Password",
-            leadingIcon = Icons.Default.Lock,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 6.dp),
-            visualTransformation = PasswordVisualTransformation(),
-        )
+        Spacer(modifier = Modifier.padding(4.dp))
+
+        Text(text = "Forgot Password?",
+            modifier = Modifier.align(Alignment.End))
+
+        Spacer(modifier = Modifier.padding(16.dp))
+
         Button(
             onClick = {
-                if (firstName.isNotEmpty() && lastName.isNotEmpty() && email.isNotEmpty() && phoneNumber.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty())
+                if(email.isNotEmpty() && password.isNotEmpty())
                 {
-
-                    if(password == confirmPassword){
-
-                        Toast.makeText(context, "SignUp Successful", Toast.LENGTH_SHORT).show()
-                    }
-                    else{
-                        Toast.makeText(context,"Password and Confirm Password does not match ", Toast.LENGTH_SHORT).show()
-                    }
+                    //verify the user
+                    Toast.makeText(context,"Login Successful", Toast.LENGTH_SHORT).show()
                 }
                 else{
-                    Toast.makeText(context, "Please fill all the fields", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(context,"Please fill all the fields", Toast.LENGTH_SHORT).show()
                 }
-                      }, modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp),
-                          shape = RoundedCornerShape(8.dp),
-            colors = ButtonDefaults.buttonColors(
-                colorResource(id = R.color.orange)
-            )
-              )
-        {
-            Text(text = "Sign Up", modifier = Modifier, color = Color.White)
+            },
+            modifier = Modifier.fillMaxWidth().padding(16.dp),
+            shape = RoundedCornerShape(8.dp),
+            colors = ButtonDefaults.buttonColors(colorResource(id = R.color.orange)),
+
+        ) {
+            Text(text = "Login", color = Color.White)
         }
+
 
         Row(
             modifier = Modifier, verticalAlignment = Alignment.CenterVertically
         ) {
-            Text(text = "Already have an account?")
+            Text(text = "Do not have an account?")
             TextButton(onClick = {
-                //navigate to login screen
-            }) { Text(text = "Login",
+                //navigate to Signup Screen
+            }) { Text(text = "SignUp",
                 color = colorResource(id = R.color.orange)) }
         }
 
@@ -173,7 +132,7 @@ fun SignUpScreen(){
         {},
             modifier = Modifier.fillMaxWidth().padding(8.dp),
             shape = RoundedCornerShape(8.dp),
-            ){
+        ){
             Image(painter = painterResource(R.drawable.google), contentDescription = null,
                 modifier = Modifier.size(24.dp))
 
@@ -181,6 +140,7 @@ fun SignUpScreen(){
 
             Text(text = "Continue with Google")
         }
+
 
 
     }
